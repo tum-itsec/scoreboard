fa42e8478d9c2013733a707ea9651dbdd483ab1b
Haspamelodica <melodicahaspa@gmail.com>
2025-11-24 19:42:45 +0100

    Autograder: better logging if requests fail

--- autograder/grading.py
+++ autograder/grading.py
@@ -45,6 +45,9 @@ while True:
     for s in submissions:
         logging.info(f"Testing submission ID {s['id']} filename {s['filename']}")
         r = requests.get(f"{HOST}/autograde/{s['id']}?APIKEY={APIKEY}")
+        if r.status_code != 200:
+            logging.error(f"Scoreboard answered with invalid statuscode to download request: {r.status_code} Server broken?")
+            break
 
         filename = os.path.basename(s['filename'])
         if not filename.endswith(".py") and not filename.endswith(".zip"):
@@ -52,46 +55,48 @@ while True:
             continue
 
         sandbox_dir = tempfile.mkdtemp(prefix="sandbox_")
-
-        filename = filename.replace("\x00", "")
-        filename = filename.replace("/", "").replace("\\", "") # Should not be necessary, but better safe than sorry
-
-        with open(sandbox_dir + "/" + filename, "wb") as f:
-            f.write(r.content)
-
-        mnts = []
-        mnts.append(docker.types.Mount("/mnt", sandbox_dir, type="bind"))
-        c = docker_client.containers.run(IMAGE_NAME, ['/run.sh', filename],
-            user=f"{os.getuid()}:{os.getgid()}",
-            working_dir="/mnt",
-            mounts=mnts,
-            network_mode="host",
-            detach=True,
-            init=True)
         try:
-            state = c.wait(timeout=TIMEOUT)
-            buf = b""
-            SIZE_LIMIT = 8 * 1024
-            for chunk in c.logs(stream=True):
-                buf += chunk
-                if len(buf) > SIZE_LIMIT:
-                    buf = buf[:SIZE_LIMIT]
-                    buf += b"\n\n[Log truncated]"
-                    break
-            output = buf.decode(errors="replace")
-        except requests.exceptions.ConnectionError:
-            logging.info(f"Terminated testing of submission {s['id']}")
-            output = f"Execution took more than {TIMEOUT} secondes, aborting..."
-            c.kill()
-        c.remove()
+            filename = filename.replace("\x00", "")
+            filename = filename.replace("/", "").replace("\\", "") # Should not be necessary, but better safe than sorry
 
-        # flag = flag_regex.search(output)
-        answer = {
-                "output": "\n".join(output.splitlines()[-100:]),
-        }
-        # print(output)
-        r = requests.post(f"{HOST}/autograde/{s['id']}?APIKEY={APIKEY}", data=answer)
-        print(r.text)
+            with open(sandbox_dir + "/" + filename, "wb") as f:
+               f.write(r.content)
 
-        shutil.rmtree(sandbox_dir)
+            mnts = []
+            mnts.append(docker.types.Mount("/mnt", sandbox_dir, type="bind"))
+            c = docker_client.containers.run(IMAGE_NAME, ['/run.sh', filename],
+                user=f"{os.getuid()}:{os.getgid()}",
+                working_dir="/mnt",
+                mounts=mnts,
+                network_mode="host",
+                detach=True,
+                init=True)
+            try:
+                state = c.wait(timeout=TIMEOUT)
+                buf = b""
+                SIZE_LIMIT = 8 * 1024
+                for chunk in c.logs(stream=True):
+                    buf += chunk
+                    if len(buf) > SIZE_LIMIT:
+                        buf = buf[:SIZE_LIMIT]
+                        buf += b"\n\n[Log truncated]"
+                        break
+                output = buf.decode(errors="replace")
+            except requests.exceptions.ConnectionError:
+                logging.info(f"Terminated testing of submission {s['id']}")
+                output = f"Execution took more than {TIMEOUT} secondes, aborting..."
+                c.kill()
+            c.remove()
+
+            # flag = flag_regex.search(output)
+            answer = {
+                    "output": "\n".join(output.splitlines()[-100:]),
+            }
+            # print(output)
+            r = requests.post(f"{HOST}/autograde/{s['id']}?APIKEY={APIKEY}", data=answer)
+            print(r.text)
+            if r.status_code != 200:
+                logging.error(f"Scoreboard answered with invalid statuscode to download request: {r.status_code} Server broken?")
+        finally:
+            shutil.rmtree(sandbox_dir)
     time.sleep(INTERVAL)
